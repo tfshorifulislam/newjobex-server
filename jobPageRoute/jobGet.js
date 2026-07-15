292ec99d6c821ef62f3a6475302a9abac1570ec9
tfshorifulislam <tfshorifulislam@gmail.com>
2026-07-15 06:19:45 +0600

    get all jobs route update query

--- jobPageRoute/jobGet.js
+++ jobPageRoute/jobGet.js
@@ -7,25 +7,47 @@ module.exports = (jobsCollection) => {
 
         const search = req.query.search || "";
         const location = req.query.location || "";
+        const workplaceType = req.query.workplaceType || "";
+        const jobType = req.query.jobType || "";
+        const postedWithin = req.query.postedWithin || ""; 
+
+        console.log("search:", search);
+        console.log("location:", location);
 
         const query = {};
 
         if (search) {
-            query.title = {
+            query.jobTitle = {
                 $regex: search,
-                $options: 'i'
-            }
+                $options: "i",
+            };
         }
 
         if (location) {
             query.location = {
                 $regex: location,
-                $options: 'i'
-            }
+                $options: "i",
+            };
         }
 
+        if (workplaceType) {
+            query.workplaceType = workplaceType;
+        }
+
+        if (jobType) {
+            query.jobType = jobType;
+        }
+
+        if (postedWithin && postedWithin !== "All") {
+            query.postedWithin = postedWithin;
+        }
+
+        console.log(query);
+
         const cursor = await jobsCollection.find(query);
+        console.log(cursor);
         const result = await cursor.toArray();
+        console.log(result);
 
         res.send(result);
     })
--- verifyRoute/sendEmail.js
+++ verifyRoute/sendEmail.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
+
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
